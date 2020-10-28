b7fa0d4497fbf5141c9f30767c471f94fd75226a
Oleg Kadyrov <abathojalyk@gmail.com>
2020-10-28 14:28:44 -0500

    added directories

--- wdio.conf.js
+++ wdio.conf.js
@@ -17,7 +17,8 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './test/*.js'
+        './test/smoke/*.js',
+        './test/regression/*.js'
     ],
     // Patterns to exclude.
     exclude: [
