2a421467d0c9248b6d5819fd182acc1d050d2026
Oleg Kadyrov <abathojalyk@gmail.com>
2020-11-04 19:01:08 -0600

    add inputValues4andClick

--- test/smoke/mainFunctionality.js
+++ test/smoke/mainFunctionality.js
@@ -1,6 +1,8 @@
 const sel = require ('../../data/selectors.json');
 const data = require ('../../data/testData.json');
 const inputValues4 = require ('../../helpers/inputValues4');
+const inputValues4andClick = require ('../../helpers/inputValues4andClick');
+
 describe('Checking the main functionality', function () {
     describe('Happy path', function () {
         it('TC-021 Create button is clickable after 1-4 are filled in', function () {
@@ -13,8 +15,7 @@ describe('Checking the main functionality', function () {
     describe('Other paths', function () {
         it('TC-022 gender he is working', function () {
             browser.url('');
-            inputValues4(data.name, data.gender.he, data.age, data.storyType);
-            $(sel.create).click();
+            inputValues4andClick(data.name, data.gender.he, data.age, data.storyType);
             const btn = $(sel.tryAgain).isDisplayed();
             expect(btn).toEqual(true);
         });
